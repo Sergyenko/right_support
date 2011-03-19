9d1c1a65905fa9f43bffdde51b568450c9971ed3
Tony Spataro <tony@rightscale.com>
2011-03-19 12:47:30 -0700

    Add some interface-compat stuff to FilterLogger.

--- lib/right_support/filter_logger.rb
+++ lib/right_support/filter_logger.rb
@@ -2,6 +2,7 @@ module RightSupport
   class FilterLogger < Logger
     def initialize(actual_logger)
       @actual_logger = actual_logger
+
     end
 
     def add(severity, message = nil, progname = nil, &block)
@@ -24,13 +25,39 @@ module RightSupport
       @actual_logger << msg
     end
 
+    def close
+      @actual_logger.close
+    end
+
     def level
       @actual_logger.level
     end
 
-    def close
-      @actual_logger.close
+    def level=(new_level)
+      @actual_logger.level = new_level
     end
+
+    # Returns +true+ iff the current severity level allows for the printing of
+    # +DEBUG+ messages.
+    def debug?; @actual_logger.debug?; end
+
+    # Returns +true+ iff the current severity level allows for the printing of
+    # +INFO+ messages.
+    def info?; @actual_logger.info?; end
+
+    # Returns +true+ iff the current severity level allows for the printing of
+    # +WARN+ messages.
+    def warn?; @actual_logger.warn?; end
+
+    # Returns +true+ iff the current severity level allows for the printing of
+    # +ERROR+ messages.
+    def error?; @actual_logger.error?; end
+
+    # Returns +true+ iff the current severity level allows for the printing of
+    # +FATAL+ messages.
+    def fatal?; @actual_logger.fatal?; end
+
+    protected
 
     def filter(severity, message)
       message
