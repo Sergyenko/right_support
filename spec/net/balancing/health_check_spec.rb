7aff6b431e348458a53050aa0402bd12b6bae6b6
Tony Spataro <tony@rightscale.com>
2011-08-15 17:45:15 -0700

    Change the wording on some specs. Add a few more pending tests. Add an rconf file to avoid confusion with multiple RVMs/gemsets.

--- spec/net/balancing/health_check_spec.rb
+++ spec/net/balancing/health_check_spec.rb
@@ -5,6 +5,46 @@ describe RightSupport::Net::Balancing::HealthCheck do
 
   end
 
+  context :good do
+    context 'given a red server' do
+      it 'does nothing' do
+        pending
+      end
+    end
+
+    context 'given a yellow-N server' do
+      it 'decreases the yellow level to N-1' do
+        pending
+      end
+
+      it 'changes to green if N == 0' do
+        pending
+      end
+    end
+  end
+
+  context :bad do
+    context 'given a green server' do
+      it 'changes to yellow-1'
+    end
+
+    context 'given a yellow-N server' do
+      it 'increases the yellow level to N+1' do
+        pending
+      end
+
+      it 'changes to red if N >= @max_failures' do
+        pending
+      end
+    end
+
+    context 'given a red server' do
+      it 'does nothing' do
+        pending
+      end
+    end
+  end
+
   context :next do
     before(:each) do
       @endpoints = [1,2,3,4,5]
@@ -12,7 +52,7 @@ describe RightSupport::Net::Balancing::HealthCheck do
       @trials = 2500
     end
 
-    context 'with all servers up' do
+    context 'given all green servers' do
       it 'chooses fairly' do
         test_random_distribution do 
           @policy.next
@@ -20,29 +60,30 @@ describe RightSupport::Net::Balancing::HealthCheck do
       end
     end
 
-    context 'with some servers down' do
-      it 'chooses only running servers' do
-        pending
-      end
-
-      it 'chooses fairly from the running servers' do
-        pending
-      end
-      
-    end
-
-    context 'with some servers marked as red' do
-      context 'when @reset_time passes for one red server' do
-        it 'resets that server to yellow-N' do
-          pending
-        end
-      end
-    end
-
-    context 'with all servers down' do
+    context 'given all red servers' do
       it 'returns nil to indicate no servers are available' do
         #@endpoints.each {|e| @policy.bad(e,0,Time.now + 1000)}
         #@policy.next(@endpoints).should be_nil
+        pending
+      end
+    end
+
+    context 'given a mixture of servers' do
+      it 'never chooses red servers' do
+        pending
+      end
+
+      it 'chooses fairly from the green and yellow servers' do
+        pending
+      end
+
+      it 'demands a health check for yellow servers' do
+        pending
+      end
+    end
+
+    context 'when @reset_time has passed since a server became red' do
+      it 'resets the server to yellow' do
         pending
       end
     end
