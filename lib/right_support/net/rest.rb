666e049fb2cf9f13cfb09ca0e17f3ad552396722
Sergey Sergyenko <sergey.sergyenko@gmail.com>
2011-08-19 15:56:49 +0300

    Added RightSupport::Net::HTTPClient instance oriented wrapper for RestClient gem

--- lib/right_support/net/rest.rb
+++ lib/right_support/net/rest.rb
@@ -14,90 +14,83 @@ module RightSupport::Net
   # REST support is merely a wrapper around other libraries, it cannot work in isolation. See the REST
   # module for more information about supported providers.
   class NoProvider < Exception; end
-
+  
   #
   # A wrapper for the rest-client gem that provides timeouts and other useful features while preserving
-  # the simplicity and ease of use of RestClient's simple, static (module-level) interface.
+  # the simplicity and ease of use of RestClient's simple, static (class-level) interface.
   #
   # Even though this code relies on RestClient, the right_support gem does not depend on the rest-client
-  # gem because not all users of right_support will want to make use of this interface. If one of REST's
-  # method is called and RestClient is not available, an exception will be raised.
+  # gem because not all users of right_support will want to make use of this interface. If one of HTTPClient
+  # instance's method is called and RestClient is not available, an exception will be raised.
   #
   #
-  # This module supports a subset of the module methods provided by RestClient and is interface-compatible
-  # with those methods it replaces; the only difference is that the REST version of each method accepts an
+  # HTTPClient supports a subset of the module methods provided by RestClient and is interface-compatible
+  # with those methods it replaces; the only difference is that the HTTPClient version of each method accepts an
   # additional, optional parameter which is a request timeout in seconds. The RestClient gem does not allow
-  # timeouts without instantiating a "heavyweight" REST client object.
+  # timeouts without instantiating a "heavyweight" HTTPClient object.
+  #
+  #   # create an instance ot HTTPClient
+  #   @client = HTTPClient.new()
   #
   #   # GET
-  #   xml = REST.get 'http://example.com/resource'
+  #   xml = @client.get 'http://example.com/resource'
   #   # and, with timeout of 5 seconds...
-  #   jpg = REST.get 'http://example.com/resource', :accept => 'image/jpg', 5
+  #   jpg = @client.get 'http://example.com/resource', {:accept => 'image/jpg', :timeout => 5}
   #
   #   # authentication and SSL
-  #   REST.get 'https://user:password@example.com/private/resource'
+  #   @client.get 'https://user:password@example.com/private/resource'
   #
   #   # POST or PUT with a hash sends parameters as a urlencoded form body
-  #   REST.post 'http://example.com/resource', :param1 => 'one'
+  #   @client.post 'http://example.com/resource', {:param1 => 'one'}
   #
   #   # nest hash parameters, add a timeout of 10 seconds (and specify "no extra headers")
-  #   REST.post 'http://example.com/resource', :nested => { :param1 => 'one' }, {}, 10
+  #   @client.post 'http://example.com/resource', {:payload => {:nested => {:param1 => 'one'}}, :timeout => 10}
   #
   #   # POST and PUT with raw payloads
-  #   REST.post 'http://example.com/resource', 'the post body', :content_type => 'text/plain'
-  #   REST.post 'http://example.com/resource.xml', xml_doc
-  #   REST.put 'http://example.com/resource.pdf', File.read('my.pdf'), :content_type => 'application/pdf'
+  #   @client.post 'http://example.com/resource', {:payload => 'the post body', :headers => {:content_type => 'text/plain'}}
+  #   @client.post 'http://example.com/resource.xml', {:payload => xml_doc}
+  #   @client.put 'http://example.com/resource.pdf', {:payload => File.read('my.pdf'), :headers => {:content_type => 'application/pdf'}}
   #
   #   # DELETE
-  #   REST.delete 'http://example.com/resource'
+  #   @client.delete 'http://example.com/resource'
   #
   #   # retrieve the response http code and headers
-  #   res = REST.get 'http://example.com/some.jpg'
+  #   res = @client.get 'http://example.com/some.jpg'
   #   res.code                    # => 200
   #   res.headers[:content_type]  # => 'image/jpg'
-  module REST
+  class HTTPClient
+    
     DEFAULT_TIMEOUT = 5
     
-    class << self
-      # Wrapper around RestClient.get -- see class documentation for details.
-      def get(url, options={}, &block)
-        query(:get, url, options, &block)
+    def initialize(options = {})
+      [:get, :post, :put, :delete].each do |method|
+         define_instance_method(method) {|*args| query(method, *args)}
       end
-      
-      # Wrapper around RestClient.get -- see class documentation for details.
-      def post(url, payload, headers={}, timeout=DEFAULT_TIMEOUT, &block)
-        request(:method=>:post, :url=>url, :payload=>payload,
-                :timeout=>timeout, :headers=>headers, &block)
-      end
+    end
 
-      # Wrapper around RestClient.get -- see class documentation for details.
-      def put(url, payload, headers={}, timeout=DEFAULT_TIMEOUT, &block)
-        request(:method=>:put, :url=>url, :payload=>payload,
-                :timeout=>timeout, :headers=>headers, &block)
+    protected
+    
+    # Helps to add default methods to class
+    def define_instance_method(method, &block)
+      (class << self; self; end).module_eval do
+        define_method(method, &block)
       end
-
-      # Wrapper around RestClient.get -- see class documentation for details.
-      def delete(url, headers={}, timeout=DEFAULT_TIMEOUT, &block)
-        request(:method=>:delete, :url=>url, :timeout=>timeout, :headers=>headers, &block)
-      end
+    end
       
-      protected 
-      
-      def query(type, url, options, &block)
-        options[:timeout] ||= DEFAULT_TIMEOUT
-        options[:headers] ||= {}
-        options.merge!(:method => type, :url => url)
-        request(options, &block)
-      end
+    def query(type, url, options={}, &block)
+      options[:timeout] ||= DEFAULT_TIMEOUT
+      options[:headers] ||= {}
+      options.merge!(:method => type, :url => url)
+      request(options, &block)
+    end
 
-      # Wrapper around RestClient::Request.execute -- see class documentation for details.
-      def request(options, &block)
-        if HAS_REST_CLIENT
-          RestClient::Request.execute(options, &block)
-        else
-          raise NoProvider, "Cannot find a suitable HTTP client library"
-        end
+    # Wrapper around RestClient::Request.execute -- see class documentation for details.
+    def request(options, &block)
+      if HAS_REST_CLIENT
+        RestClient::Request.execute(options, &block)
+      else
+        raise NoProvider, "Cannot find a suitable HTTP client library"
       end
-    end #class << self
-  end
+    end
+  end# HTTPClient
 end
--- spec/net/rest_spec.rb
+++ spec/net/rest_spec.rb
@@ -1,37 +1,38 @@
 require 'spec_helper'
 
-describe RightSupport::Net::REST do
+describe RightSupport::Net::HTTPClient do
   it 'is interface-compatible with RestClient' do
-    m = RightSupport::Net::REST
-    m.should respond_to(:get)
-    m.should respond_to(:post)
-    m.should respond_to(:put)
-    m.should respond_to(:delete)
+    @client = RightSupport::Net::HTTPClient.new()
+    @client.should respond_to(:get)
+    @client.should respond_to(:post)
+    @client.should respond_to(:put)
+    @client.should respond_to(:delete)
   end
 
   context :request do
     before(:each) do
       r = 'this is a short mock REST response'
       flexmock(RestClient::Request).should_receive(:execute).and_return(r).by_default
+      @client = RightSupport::Net::HTTPClient.new()
     end
 
     context 'given just a URL' do
       it 'succeeds' do
-        p = {:method=>:get, :timeout=>RightSupport::Net::REST::DEFAULT_TIMEOUT,
+        p = {:method=>:get, :timeout=>RightSupport::Net::HTTPClient::DEFAULT_TIMEOUT,
              :url=>'/moo', :headers=>{}}
         flexmock(RestClient::Request).should_receive(:execute).with(p)
 
-        RightSupport::Net::REST.get('/moo')
+        @client.get('/moo')
       end
     end
 
     context 'given a URL and headers' do
       it 'succeeds' do
-        p = {:method=>:get, :timeout=>RightSupport::Net::REST::DEFAULT_TIMEOUT,
+        p = {:method=>:get, :timeout=>RightSupport::Net::HTTPClient::DEFAULT_TIMEOUT,
              :url=>'/moo', :headers=>{:mrm=>1, :blah=>:foo}}
         flexmock(RestClient::Request).should_receive(:execute).with(p)
 
-        RightSupport::Net::REST.get('/moo', {:headers => {:mrm=>1, :blah=>:foo}})
+        @client.get('/moo', {:headers => {:mrm=>1, :blah=>:foo}})
       end
     end
 
@@ -42,17 +43,17 @@ describe RightSupport::Net::REST do
              :url=>'/moo', :headers=>{}}
         flexmock(RestClient::Request).should_receive(:execute).with(p)
 
-        RightSupport::Net::REST.get('/moo', {:timeout => 42})
+        @client.get('/moo', {:timeout => 42})
       end
     end
     
     context 'given a URL and any other parameters' do
       it 'succeeds' do
-        p = {:method=>:get, :timeout=>RightSupport::Net::REST::DEFAULT_TIMEOUT,
+        p = {:method=>:get, :timeout=>RightSupport::Net::HTTPClient::DEFAULT_TIMEOUT,
              :url=>'/moo', :headers=>{},:open_timeout => 1, :foo => :bar}
         flexmock(RestClient::Request).should_receive(:execute).with(p)
 
-        RightSupport::Net::REST.get('/moo', {:open_timeout => 1, :foo => :bar})
+        @client.get('/moo', {:open_timeout => 1, :foo => :bar})
       end
     end
   end
