2e613845ead712602f3a1e519beef896a32610a1
Tony Spataro <tony@rightscale.com>
2011-10-06 22:27:50 -0700

    Fix system logger spec.

--- spec/log/system_logger_spec.rb
+++ spec/log/system_logger_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'RightSupport::Log::SystemLogger' do
   before(:each) do
-    pending('unsupported on this platform') unless defined?(RightSupport::SystemLogger)
+    pending('unsupported on this platform') unless defined?(RightSupport::Log::SystemLogger)
   end
 
   it 'faithfully translates log levels' do
@@ -10,14 +10,14 @@ describe 'RightSupport::Log::SystemLogger' do
                :info=>Logger::INFO, :warn=>Logger::WARN,
                :error=>Logger::ERROR, :fatal=>Logger::FATAL }
     levels.each_pair do |level, syslog_level|
-      @logger = RightSupport::SystemLogger.new('spec')
+      @logger = RightSupport::Log::SystemLogger.new('spec')
       flexmock(@logger).should_receive(:emit_syslog).with(syslog_level, 'moo bah oink')
       @logger.send(level, 'moo bah oink')
     end
   end
 
   it 'escapes % characters to avoid confusing printf()' do
-    @logger = RightSupport::SystemLogger.new('spec')
+    @logger = RightSupport::Log::SystemLogger.new('spec')
     flexmock(@logger).should_receive(:emit_syslog).with(Integer, 'All systems 100%% -- %%licious!')
 
     @logger.info('All systems 100% -- %licious!')
@@ -25,14 +25,14 @@ describe 'RightSupport::Log::SystemLogger' do
 
   context ':split option' do
     it 'when true, splits multi-line messages' do
-      @logger = RightSupport::SystemLogger.new('spec', :split=>true)
+      @logger = RightSupport::Log::SystemLogger.new('spec', :split=>true)
       flexmock(@logger).should_receive(:emit_syslog).times(5)
 
       @logger.info("This is a\nmulti line\r\nlog message\n\rwith all kinds\n\n\rof stuff")
     end
 
     it 'when false, passes through multi-line messages' do
-      @logger = RightSupport::SystemLogger.new('spec', :split=>false)
+      @logger = RightSupport::Log::SystemLogger.new('spec', :split=>false)
       flexmock(@logger).should_receive(:emit_syslog).times(1)
 
       @logger.info("This is a\nmulti line\r\nlog message\n\rwith all kinds\n\n\rof stuff")
@@ -41,14 +41,14 @@ describe 'RightSupport::Log::SystemLogger' do
 
   context ':color option' do
     it 'when true, passes through ANSI color codes' do
-      @logger = RightSupport::SystemLogger.new('spec', :color=>true)
+      @logger = RightSupport::Log::SystemLogger.new('spec', :color=>true)
       flexmock(@logger).should_receive(:emit_syslog).with(Integer, /[\e]/)
 
       @logger.info("This has \e[16;32mcolor\e[7;0m inside it!")
     end
 
     it 'when false, strips out ANSI color codes' do
-      @logger = RightSupport::SystemLogger.new('spec', :color=>false)
+      @logger = RightSupport::Log::SystemLogger.new('spec', :color=>false)
       flexmock(@logger).should_receive(:emit_syslog).with(Integer, /[^\e]/)
 
       @logger.info("This has \e[16;32mcolor\e[7;0m inside it!")
